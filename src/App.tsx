7ca39a2c2c5ad67a499e581fa2a8f43eddd3a393
quingsley <jeromejuma25@gmail.com>
2024-06-01 06:40:40 +0300

    feat: Add authentication page to the router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { Layout } from "./layout";
+import Auth from "./pages/auth/auth_page";
 import { LandingPage } from "./pages/landing-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const routes = createBrowserRouter([{ path: "/", element: <Layout />, children: [{ path: "/", element: <LandingPage /> }] }]);
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+    ],
+  },
+]);
 function App() {
   return <RouterProvider router={routes} />;
 }
