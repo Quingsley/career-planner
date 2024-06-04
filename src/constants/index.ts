6078d87bc89109be2a31c8df06c28ad2b791f554
quingsley <jeromejuma25@gmail.com>
2024-06-04 06:50:08 +0300

    feat: Add constants for mutex and base URL
    This commit adds constants for a mutex and a base URL. The mutex is used for synchronization purposes, and the base URL is set to "http://192.168.0.104:4000". These constants will be used throughout the application for managing concurrency and making API requests.

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+import { Mutex } from "async-mutex";
+let MUTEX: Mutex | undefined;
+
+export function getMutex() {
+  if (MUTEX === undefined) {
+    MUTEX = new Mutex();
+  }
+  return MUTEX;
+}
+
+export const BASE_URL = "http://192.168.0.104:4000";
