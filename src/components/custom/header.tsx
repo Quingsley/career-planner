3bd8fc390e5af0066b58def4d46c0b5e86859963
quingsley <jeromejuma25@gmail.com>
2024-06-05 07:19:51 +0300

    feat: Add UI components for Skeleton and Badge
    This commit adds two new UI components, Skeleton and Badge, to the codebase. The Skeleton component is used for displaying loading placeholders, while the Badge component is used for displaying various types of badges. These components will enhance the user interface and provide better visual feedback to the users.

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -1,25 +1,33 @@
+import { useAppSelector } from "../../hooks";
 import { BriefcaseIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export function Header() {
+  const user = useAppSelector(state => state.user);
   return (
     <header className="px-4 lg:px-6 h-14 flex flex-row items-center">
       <Link className="flex items-center justify-center" to="/">
-        {/*className="h-6 w-6"   */}
         <BriefcaseIcon />
         <span className="sr-only">Career Planner</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-sm font-medium hover:underline underline-offset-4" to="#">
+        <Link className="text-sm font-medium hover:underline underline-offset-4" to="#features">
           Features
         </Link>
         <Link className="text-sm font-medium hover:underline underline-offset-4" to="#">
           About
         </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" to="#">
-          Contact
+        {user.userId && (
+          <Link className="text-sm font-medium hover:underline underline-offset-4" to="/dashboard">
+            Dashboard
+          </Link>
+        )}
+        <Link className="text-sm font-medium hover:underline underline-offset-4" to={`${user.userId ? "/auth" : "/"}`}>
+          {user.userId ? "LOG OUT" : "Login"}
         </Link>
       </nav>
     </header>
   );
 }
+
+// ADD log out functionality
