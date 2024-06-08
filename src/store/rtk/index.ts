ae47448e765e2754c5514f0b4da3d2c0f6536326
quingsley <jeromejuma25@gmail.com>
2024-06-08 06:45:22 +0300

    chore: Update redux-persist dependency and handle data purging in slices

--- src/store/rtk/index.ts
+++ src/store/rtk/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth";
 export * from "./profile-api";
 export * from "./ai-flows";
+export * from "./public-ai-flow";
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistCombineReducers } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import { authApi, profileApi, aiFlowsApi } from "./rtk";
+import { authApi, profileApi, aiFlowsApi, publicAiFlowApi } from "./rtk";
 import { errorReducer } from "./slices/error-slice";
 import { profileReducer } from "./slices/profile-slice";
-import { userReducer } from "./slices/user-slice";
+import { login, userReducer } from "./slices/user-slice";
 import { careerReducer } from "./slices/career-slice";
 
 const persistConfig = {
@@ -21,6 +21,7 @@ const rootReducer = {
   [authApi.reducerPath]: authApi.reducer,
   [profileApi.reducerPath]: profileApi.reducer,
   [aiFlowsApi.reducerPath]: aiFlowsApi.reducer,
+  [publicAiFlowApi.reducerPath]: publicAiFlowApi.reducer,
 };
 
 const persistedReducer = persistCombineReducers(persistConfig, rootReducer);
@@ -30,8 +31,25 @@ export const store = configureStore({
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(authApi.middleware).concat(profileApi.middleware).concat(aiFlowsApi.middleware),
+    getDefaultMiddleware({ serializableCheck: false, immutableCheck: false })
+      .prepend((_: any) => (next: Function) => (action: any) => logOut(action, next))
+      .concat(authApi.middleware)
+      .concat(profileApi.middleware)
+      .concat(aiFlowsApi.middleware)
+      .concat(publicAiFlowApi.middleware),
 });
+
+function logOut(action: Action, next: Function) {
+  if (action.type === "CLEAR_APP") {
+    console.log("purging state now...");
+    // purgeStoredState(persistConfig);
+    persistor.purge().then(() => {
+      console.log("🚀🚀🚀 APP RESET PERFORMED");
+      return next(login(false));
+    });
+  }
+  return next(action);
+}
 
 export const persistor = persistStore(store);
 
