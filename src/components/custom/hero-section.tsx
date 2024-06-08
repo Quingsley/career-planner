22f22c7039d2028d8438a3c26ccf94050381f5ff
quingsley <jeromejuma25@gmail.com>
2024-06-08 06:42:11 +0300

    feat: Add demo button to hero section for trying out the CV uploader

--- src/components/custom/hero-section.tsx
+++ src/components/custom/hero-section.tsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
 
 export function HeroSection() {
+  const navigation = useNavigate();
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">Unlock Your Career Potential</h1>
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                Unlock Your Career Potential
+              </h1>
               <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-                Explore your skills, discover personalized career paths, and get support for your job search with our AI-powered career planning
-                platform.
+                Explore your skills, discover personalized career paths, and get support for your job search with our
+                AI-powered career planning platform.
               </p>
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
@@ -20,6 +24,9 @@ export function HeroSection() {
               >
                 Get Started
               </Link>
+              <Button onClick={() => navigation("/cv-uploader")} variant="outline">
+                Try A demo
+              </Button>
             </div>
           </div>
           <img
